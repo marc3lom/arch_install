2b567710c2f912c6c89072dcd4d62c0f0559e568
Marcelo Martinelli <mmartinelli@gmail.com>
2021-04-03 13:19:06 -0300

    Add files via upload

---
+++ xmonad.hs
@@ -0,0 +1,29 @@
+import	XMonad
+
+import	XMonad.Config.Desktop
+
+import	XMonad.Hooks.DynamicLog
+import	XMonad.Hooks.ManageDocks
+
+import	XMonad.Util.EZConfig
+import	XMonad.Util.Run
+
+import	System.IO
+
+main = do
+	xmproc <- spawnPipe "xmobar /home/marcelom/.config/xmobar/xmobarrc"
+	xmonad $ docks defaultConfig
+		{ manageHook	=	manageDocks <+> manageHook defaultConfig
+		, layoutHook	=	avoidStruts $ layoutHook defaultConfig
+		, logHook	=	dynamicLogWithPP xmobarPP
+					{ ppOutput	=	hPutStrLn xmproc
+					, ppTitle	=	xmobarColor "green"  "" . shorten 50
+					}
+		, terminal	=	myTerminal
+		, modMask	=	myModMask
+		, borderWidth	=	myBorderWidth
+		}
+
+myTerminal 	= "terminator"
+myModMask 	= mod4Mask
+myBorderWidth 	= 3
